5e01a068bce6593cf296f8ecfc48d8a7d6051a02
wanghan <1431909656@qq.com>
2021-12-18 17:18:23 +0800

    登录接口实现

--- prevention/vue_epidemic/src/main.js
+++ prevention/vue_epidemic/src/main.js
@@ -6,7 +6,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementUi from 'element-ui'
-import {Message} from 'element-ui'
+import {
+    Message
+} from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import VCharts from 'v-charts'
 import 'v-charts/lib/pie.common'
@@ -14,46 +16,52 @@ Vue.use(VCharts)
 Vue.use(ElementUi)
 
 
-import  io from './components/socket/weapp.socket.io'
+import io from './components/socket/weapp.socket.io'
 //套接字端口定义
-Vue.prototype.socket=io('http://localhost:5200')
+Vue.prototype.socket = io('http://localhost:5200')
 
 // 引用vue-socket.io
 import VueSocket from 'vue-socket.io'
 Vue.use(new VueSocket({
     debug: false,
-    connection:'http://localhost:5200'
+    connection: 'http://localhost:5200'
 }))
 
 
-Vue.prototype.socketpath="http://localhost:5200",
+Vue.prototype.socketpath = "http://localhost:5200",
 
-Vue.prototype.basePath="http://localhost:3000";
-//引包http://192.168.0.22:999
+    Vue.prototype.basePath = "http://localhost:3000";
 import axios from 'axios'
+// 接口初始url
 axios.defaults.baseURL = 'http://localhost:3000';
 /**
  * 封装axios方法
  * @param options
  */
-let futureAxios = (options)=>{
-    options.data.token=window.localStorage.token;
+let futureAxios = (options) => {
+    options.data.token = window.localStorage.token;
     axios({
-      url:options.url,
-      method:options.method || 'POST',
-      data:options.data,
-      params:options.data,
-    }).then(result=>{
-      if(options.success){
-        options.success(result.data)
-      }
-    }).catch(err=>{
+        url: options.url,
+        method: options.method || 'POST',
+        data: options.data,
+        params: options.data,
+    }).then(result => {
+        if (options.success) {
+            options.success(result.data)
+        }
+    }).catch(err => {
         let msg = err.response ? err.response.data : '请求异常'
         if (options.error) {
             options.error(msg)
-            Message.error({message: msg, offset: 150});
+            Message.error({
+                message: msg,
+                offset: 150
+            });
         } else {
-            Message.error({message: msg, offset: 150});
+            Message.error({
+                message: msg,
+                offset: 150
+            });
         }
     })
 }
@@ -62,21 +70,27 @@ let futureAxios = (options)=>{
  * //封装axios方法,为不需要登录操作时使用
  * @param options 配置
  */
-let Axios= (options)=>{
+let Axios = (options) => {
     axios({
-        url:options.url,
-        method:options.method||'POST',
+        url: options.url,
+        method: options.method || 'POST',
         data: options.data,
         params: options.data
-    }).then(result=>{
-        if (options.success)  options.success(result.data)
-    }).catch(err=>{
-        let msg = err.response ? err.response.data:'请求异常'
-        if (options.error){
+    }).then(result => {
+        if (options.success) options.success(result.data)
+    }).catch(err => {
+        let msg = err.response ? err.response.data : '请求异常'
+        if (options.error) {
             options.error(msg)
-            Message.error({message: msg, offset: 150});
-        }else {
-            Message.error({message: msg, offset: 150});
+            Message.error({
+                message: msg,
+                offset: 150
+            });
+        } else {
+            Message.error({
+                message: msg,
+                offset: 150
+            });
         }
     })
 }
@@ -88,8 +102,8 @@ Vue.prototype.$Axios = Axios
 
 Vue.config.productionTip = false
 
-window.vm  = new Vue({
-  router,
-  store,
-  render: h => h(App)
+window.vm = new Vue({
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
